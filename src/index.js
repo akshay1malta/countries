6be086ce276005655b327e77730f3bdcaafbc15d
iphalgun <phalgun99@gmail.com>
2021-01-11 01:37:39 +0530

    - Country flags json and function added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 "use strict";
 
 const countriesData = require("./countriesData.json");
+const countriesFlag = require("./countriesFlags.json");
 
 exports.getAllCountriesNames = () => {
   return countriesData.map((country) => {
     return country.name;
   });
 };
+
+exports.getcountriesFlag = (countryName) => {
+  let flagIndex = countriesFlag.find(country => {
+    country.name.toLowerCase() === countryName.toLowerCase()
+  })
+  return flagIndex ? countriesFlag[flagIndex].emoji : undefined;
+}
--- tesCases/index.test.js
+++ tesCases/index.test.js
@@ -1,8 +1,15 @@
-const { getAllCountriesNames } = require("../src/index");
+const { getAllCountriesNames, getCountryFlag } = require("../src/index");
 var countriesData = require("../src/countriesData.json");
+var countryFlag = require("../src/countryFlags.json");
 
 test("Returns all countries", () => {
   expect(getAllCountriesNames()).toEqual(countriesData.map((country) => {
     return country.name;
   }));
 });
+
+test("Returns country flag", () => {
+  const countryName = "India";
+  const countryFlag = "🇮🇳";
+  expect(getCountryFlag(countryName)).toBe(countryFlag)
+})
