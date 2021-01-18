cb6437d157abd5a84c13e6eea796e37e0887bbd0
iphalgun <phalgun99@gmail.com>
2021-01-18 15:46:51 +0530

    - Country calling codes added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 "use strict";
 
 const countriesData = require("./countriesData.json");
+const countryCallingCodes = require("./countryCallingCode.json");
 
 exports.getAllCountriesNames = () => {
   return countriesData.map((country) => {
     return country.name;
+  });
+};
+
+exports.getAllCountriesCallingCode = () => {
+  return countryCallingCodes.map((country) => {
+    return country.dial_code;
   });
 };
--- tesCases/index.test.js
+++ tesCases/index.test.js
@@ -1,8 +1,15 @@
-const { getAllCountriesNames } = require("../src/index");
+const { getAllCountriesNames, getAllCountriesCallingCode } = require("../src/index");
 var countriesData = require("../src/countriesData.json");
+var countryCallingCodes = require("../src/countryCallingCode.json");
 
 test("Returns all countries", () => {
   expect(getAllCountriesNames()).toEqual(countriesData.map((country) => {
     return country.name;
+  }));
+});
+
+test("Returns all country codes", () => {
+  expect(getAllCountriesCallingCode()).toEqual(countryCallingCodes.map((country) => {
+    return country.dial_code;
   }));
 });
