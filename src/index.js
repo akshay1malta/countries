a98eb0eed65a9d088c41eb8fb02fb3a182f5bf5e
Alam khan <alam@stack-avenue.com>
2021-01-19 09:39:26 +0530

    - Included currency module and test cases related to it

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 "use strict";
 
 const countriesData = require("./countriesData.json");
+const currencyData = require("./currencyData.json");
 
 exports.getAllCountriesNames = () => {
   return countriesData.map((country) => {
     return country.name;
   });
+};
+
+
+exports.getCurrencybyCountryCode = (countryCode, options) => {
+   return currencyData[countryCode.toUpperCase()];
 };
--- tesCases/index.test.js
+++ tesCases/index.test.js
@@ -1,8 +1,16 @@
-const { getAllCountriesNames } = require("../src/index");
+const { getAllCountriesNames, getCurrencybyCountryCode } = require("../src/index");
 var countriesData = require("../src/countriesData.json");
+const currencyData = require("./currencyData.json");
 
 test("Returns all countries", () => {
   expect(getAllCountriesNames()).toEqual(countriesData.map((country) => {
     return country.name;
   }));
 });
+/** Currency Related Test Cases start **/
+test("Returns all currencies", () => {
+  const currenCy = 'USD';
+  expect(JSON.stringify(getCurrencybyCountryCode(currenCy))).toEqual(JSON.stringify(currencyData[currenCy.toUpperCase()]));
+});
+
+/** Currency Related Test Cases end **/
