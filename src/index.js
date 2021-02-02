9a26d4e70e75fb3fe7cdb988a739f3ab1a90bacd
Ayoush <ayoushchourasia1418@gmail.com>
2021-02-02 10:44:17 +0530

    Added method to return countries and capitals

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 "use strict";
 
 const countriesData = require("./countriesData.json");
+const countriesCapitals = require("./countriesCapital.json");
 
 exports.getAllCountriesNames = () => {
   return countriesData.map((country) => {
+    console.log(country.name)
     return country.name;
+  });
+};
+
+exports.getAllCountriesCapitals = () => {
+  return countriesCapitals.map((country) => {
+    return [country.country, country.city];
   });
 };
