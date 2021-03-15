d4865ab5cb4b70faf92758052dba85638dc45935
Jeshan <jeshankhn6@gmail.com>
2021-03-15 12:12:59 +0530

    Changes made in this commit - Added a common function - Implemented factory pattern - Added a map to store functions reference - Moved all the functions in separate module

--- src/index.js
+++ src/index.js
@@ -1,77 +1,14 @@
 "use strict";
 
-const countriesData = require("./countriesData.json");
-const currencyData = require("./currencyData.json");
-const countryStates = require("./countriesState.json");
-const countriesCapitals = require("./countriesCapital.json");
-const countryCallingCodes = require("./countryCallingCode.json");
-const countriesFlag = require("./countryFlags.json");
-const countriesLanguageData = require("./countriesLanguageData.json");
-const timezoneData=require("./countryTimezone.json")
-const continentsData = require("./continentsData.json");
+const countryDetailsMap = require("./map.js");
 
-exports.getAllCountriesNames = () => {
-  return countriesData.map((country) => {
-    return country.name;
-  });
-}; 
-
-exports.getContinentName=(countryName)=>{
-  let continentIndex = continentsData.findIndex(country => { 
-   return country.country.toLowerCase() === countryName.toLowerCase()
-  })
-  return continentIndex ? continentsData[continentIndex].continent : undefined;
+const getCountryInfo = (options) => {
+  const { methodType, attributes } = options;
+  const FunctionFactory = countryDetailsMap.get(methodType);
+  return FunctionFactory(attributes);
 };
 
-
-exports.getCountryCode = (countryName)=>{
-  let CountryCodeIndex = countriesFlag.findIndex(country => {
-   return country.name.toLowerCase() === countryName.toLowerCase()
-  })
-  return CountryCodeIndex ? countriesFlag[CountryCodeIndex].code : undefined;
-}
-
-exports.getCountryTimeZone = (countryName)=>{
-  let timeZoneIndex =timezoneData.findIndex(country=>{
-    return country.name.toLowerCase()==countryName.toLowerCase()
-  })
-  return timeZoneIndex!=null ? timezoneData[timeZoneIndex].timezones[0] : undefined;
-}
-
-exports.getAllCountryStateNames = () => {
-  return countryStates.map((country) => {
-    return country.states.map((states) => {
-      return country.name - states.name;
-    })
-  });
-};
-
-exports.getCurrencybyCountryCode = (countryCode, options) => {
-   return currencyData[countryCode.toUpperCase()];
-};
-
-exports.getAllCountriesCapitals = () => {
-  return countriesCapitals
-};
-
-exports.getAllCountriesCallingCode = () => {
-  return countryCallingCodes.map((country) => {
-    return country.dial_code;
-  });
-};
-
-exports.getAllCountriesFlag = () => {
-  return countriesFlag.map((country) => {
-    return country.emoji;
-  });
-};
-
-exports.getAllCountriesLanguages = () => {
-  return countriesLanguageData
-}
-
-exports.getAllIsoCodes = () => {
-  return countriesData.map((country) => {
-    return country.code;
-  });
-}
+console.log(
+  "Data:",
+  getCountryInfo({ methodType: "COUNTRY_NAMES", attributes: "india" })
+);
