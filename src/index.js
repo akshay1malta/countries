e07e293d16c47eb9782853062b97a756b36fd69c
Akshay Malta <akshay@stack-avenue.com>
2021-04-18 20:21:16 +0530

    function method added in index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,20 @@
 
 const countryDetailsMap = require("./map.js");
 const countriesData = require("./allCountriesData.json");
-const { getFilteredObj } = require("./functions.js");
+const {
+  getContinentName,
+  getCountryCode,
+  getCountryTimeZone,
+  getAllCountryStateNames,
+  getAllCountriesNames,
+  getAllCountriesLanguages,
+  getAllIsoCodes,
+  getAllCountriesFlag,
+  getAllCountriesCallingCode,
+  getAllCountriesCapitals,
+  getCurrencybyCountryCode,
+  getFilteredObj
+} = require("./functions");
 
 exports.getCountryInfo = (options) => {
   const { methodType, attributes } = options;
@@ -24,3 +37,53 @@ exports.getCountryInfo = (options) => {
   const FunctionFactory = countryDetailsMap.get(methodType);
   return FunctionFactory(attributes);
 };
+
+exports.getAllCountriesNames = () => {
+  return getAllCountriesNames()
+};
+
+exports.getContinentName = (countryName) => {
+  return getContinentName(countryName)
+};
+
+exports.getCountryCode = (countryName) => {
+  return getCountryCode(countryName)
+};
+
+exports.getCountryTimeZone = (countryName) => {
+  return getCountryTimeZone(countryName)
+};
+
+exports.getAllCountryStateNames = () => {
+  return getAllCountryStateNames()
+};
+
+exports.getCurrencybyCountryCode = (countryName) => {
+  return getCurrencybyCountryCode(countryName)
+};
+
+exports.getAllCountriesCapitals = () => {
+  return getAllCountriesCapitals()
+};
+
+exports.getAllCountriesCallingCode = () => {
+  return getAllCountriesCallingCode()
+};
+
+exports.getAllCountriesFlag = () => {
+  return getAllCountriesFlag()
+};
+
+exports.getAllCountriesLanguages = () => {
+  return getAllCountriesLanguages()
+};
+
+
+exports.getAllIsoCodes = () => {
+  return getAllIsoCodes()
+};
+
+exports.getCountryByCode = (number) => {
+  return getCountryByCode(number)
+};
+
